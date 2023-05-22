1d8f67788d5b247b986ea20b81259311bf95d047
superuser <superuser@realproger.local>
2023-05-22 15:58:55 +0500

    Done

--- Day1/exercises.md
+++ Day1/exercises.md
@@ -8,3 +8,12 @@ The numbers obtained should be printed in a comma-separated sequence on a single
 
 Consider use range(#begin, #end) method.
 
+# Question 2
+
+Write a program which can compute the factorial of a given numbers.
+The results should be printed in a comma-separated sequence on a single line.
+Suppose the following input is supplied to the program: 8 Then, the output should be:40320
+
+# Hints:
+
+In case of input data being supplied to the question, it should be assumed to be a console input.
--- Day1/solutions.py
+++ Day1/solutions.py
@@ -11,7 +11,38 @@ for i in range(2000, 3201):
 # print(','.join(array))
 
 # Solving with using generators
-print(*(i for i in range(2000, 3201) if i % 7 == 0 and i % 5 != 0))
+# print(*(i for i in range(2000, 3201) if i % 7 == 0 and i % 5 != 0))
 
 # sign "*" before statement used for unpaking ;)
 
+
+"""
+Second task
+"""
+
+def factorial(x):
+    if x == 0:
+        return 1
+    else:
+        return factorial(x - 1) * x
+
+print(factorial(8))
+
+###
+##
+"""
+The uprising function itself with the argument x - 1 and multiplies the result by x. 
+This happens until x becomes the necessary 0, 
+and eventually the factor of the original number x is returned
+"""
+##
+###
+
+# Same program/ solution with using while loop
+num = int(input("Enter your number: "))
+fact = 1
+i = 1
+while i <= num:
+    fact = fact * i;
+    i = i + 1
+# print(fact)
