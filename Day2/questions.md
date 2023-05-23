c1c5e60afb3c21678d0c14fe848199679c053e49
superuser <superuser@realproger.local>
2023-05-23 14:19:01 +0500

    6th Task

--- Day2/questions.md
+++ Day2/questions.md
@@ -37,3 +37,35 @@
 > **_Use **init** method to construct some param
 
 ---
+
+# Question 6
+
+### **Question:**
+
+> **_Write a program that calculates and prints the value according to the given formula:_**
+
+> **_Q = Square root of [(2 _ C _ D)/H]_**
+
+> **_Following are the fixed values of C and H:_**
+
+> **_C is 50. H is 30._**
+
+> **_D is the variable whose values should be input to your program in a comma-separated sequence.For example
+> Let us assume the following comma separated input sequence is given to the program:_**
+
+```
+100,150,180
+```
+
+> **_The output of the program should be:_**
+
+```
+18,22,24
+```
+
+---
+
+### Hints:
+
+> **_If the output received is in decimal form, it should be rounded off to its nearest value (for example, if the output received is 26.0, it should be printed as 26).In case of input data being supplied to the question, it should be assumed to be a console input._**
+
--- Day2/solutions.py
+++ Day2/solutions.py
@@ -27,6 +27,36 @@ class InputOutString:
     def printString(self):
         print(self.our_string.upper())
 
-object1 = InputOutString()
-object1.getString()
-object1.printString()
+# object1 = InputOutString()
+# object1.getString()
+# object1.printString()
+
+
+"""
+Task 6th
+"""
+
+from math import sqrt # import specific functions as importing all using *
+                      # is bad practice
+
+C,H = 50,30
+
+def calc(D):
+    return sqrt((2*C*D)/H)
+
+D = [int(i) for i in input().split(',')] # splits in comma position and set up in list
+D = [int(i) for i in D]   # converts string to integer
+D = [calc(i) for i in D]  # returns floating value by calc method for every item in D
+D = [round(i) for i in D] # All the floating values are rounded
+D = [str(i) for i in D]   # All the integers are converted to string to be able to apply join operation
+
+# print(",".join(D))
+
+'''    OR    '''
+
+C,H = 50,30
+
+def calc(D):
+    return sqrt((2*C*D)/H)
+
+print(",".join([str(int(calc(int(i)))) for i in input().split(',')]))
