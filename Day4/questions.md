db1905ae48476c25c913115964ca45241bb206e6
Tolbaev Muntasir <mtolbaev2000@gmail.com>
2023-05-25 12:24:59 +0500

    15th task

--- Day4/questions.md
+++ Day4/questions.md
@@ -24,3 +24,30 @@ LOWER CASE 9
 > **_In case of input data being supplied to the question, it should be assumed to be a console input._**
 
 ---
+
+
+# Question 15
+
+### **Question:**
+
+> **_Write a program that computes the value of a+aa+aaa+aaaa with a given digit as the value of a._**
+
+> **_Suppose the following input is supplied to the program:_**
+
+```
+9
+```
+
+> **_Then, the output should be:_**
+
+```
+11106
+```
+
+---
+
+### Hints:
+
+> **_In case of input data being supplied to the question, it should be assumed to be a console input._**
+
+---
--- Day4/solutions.py
+++ Day4/solutions.py
@@ -36,3 +36,24 @@ upper = sum(1 for i in word if i.isupper())           # sum function cumulativel
 lower = sum(1 for i in word if i.islower())
 
 print("UPPER CASE {0}\nLOWER CASE {1}".format(upper,lower))
+
+
+"""
+15th Task
+"""
+a = input()
+total,tmp = 0,str()        # initialing an integer and empty string
+
+for i in range(4):
+    tmp+=a               # concatenating 'a' to 'tmp'
+    total+=int(tmp)      # converting string type to integer type
+
+print(total)
+
+"""
+OTHER VARIANT
+"""
+
+a = input()
+total = int(a) + int(2*a) + int(3*a) + int(4*a)  # N*a=Na, for example  a="23", 2*a="2323",3*a="232323"
+print(total)
