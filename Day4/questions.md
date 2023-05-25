0994b88e729ac682c355857af42d55b0dbf427ee
Tolbaev Muntasir <mtolbaev2000@gmail.com>
2023-05-25 12:49:47 +0500

    16th task

--- Day4/questions.md
+++ Day4/questions.md
@@ -51,3 +51,27 @@ LOWER CASE 9
 > **_In case of input data being supplied to the question, it should be assumed to be a console input._**
 
 ---
+
+# Question 16
+
+### **Question:**
+
+> **_Use a list comprehension to square each odd number in a list. The list is input by a sequence of comma-separated numbers._** >**_Suppose the following input is supplied to the program:_**
+
+```
+1,2,3,4,5,6,7,8,9
+```
+
+> **_Then, the output should be:_**
+
+```
+1,9,25,49,81
+```
+
+---
+
+### Hints:
+
+> **_In case of input data being supplied to the question, it should be assumed to be a console input._**
+
+---
--- Day4/solutions.py
+++ Day4/solutions.py
@@ -1,59 +1,79 @@
-"""
-14th Task
-"""
+# """
+# 14th Task
+# """
 
-upper_l = 0
-lower_l = 0
-sentence = input("Enter your text:")
-for i in sentence:
-    if i.isupper():
-        upper_l += 1
-    elif i.islower():
-        lower_l += 1
-    else:
-        pass
+# upper_l = 0
+# lower_l = 0
+# sentence = input("Enter your text:")
+# for i in sentence:
+#     if i.isupper():
+#         upper_l += 1
+#     elif i.islower():
+#         lower_l += 1
+#     else:
+#         pass
 
-# print(f"UPPER CASE {upper_l}\nLOWER CASE {lower_l}")
+# # print(f"UPPER CASE {upper_l}\nLOWER CASE {lower_l}")
 
-"""
-OTHER VARIANT
-"""
+# """
+# OTHER VARIANT
+# """
 
-word = input()
-upper,lower = 0,0
+# word = input()
+# upper,lower = 0,0
 
-for i in word:
-        lower+=i.islower()
-        upper+=i.isupper()
+# for i in word:
+#         lower+=i.islower()
+#         upper+=i.isupper()
+
+# # print("UPPER CASE {0}\nLOWER CASE {1}".format(upper,lower))
+
+# """
+# OTHER VARIANT
+# """
+# word = input()
+# upper = sum(1 for i in word if i.isupper())           # sum function cumulatively sum up 1's if the condition is True
+# lower = sum(1 for i in word if i.islower())
 
 # print("UPPER CASE {0}\nLOWER CASE {1}".format(upper,lower))
 
+
+# """
+# 15th Task
+# """
+# a = input()
+# total,tmp = 0,str()        # initialing an integer and empty string
+
+# for i in range(4):
+#     tmp+=a               # concatenating 'a' to 'tmp'
+#     total+=int(tmp)      # converting string type to integer type
+
+# print(total)
+
+# """
+# OTHER VARIANT
+# """
+
+# a = input()
+# total = int(a) + int(2*a) + int(3*a) + int(4*a)  # N*a=Na, for example  a="23", 2*a="2323",3*a="232323"
+# print(total)
+
+
 """
-OTHER VARIANT
-"""
-word = input()
-upper = sum(1 for i in word if i.isupper())           # sum function cumulatively sum up 1's if the condition is True
-lower = sum(1 for i in word if i.islower())
-
-print("UPPER CASE {0}\nLOWER CASE {1}".format(upper,lower))
-
-
-"""
-15th Task
-"""
-a = input()
-total,tmp = 0,str()        # initialing an integer and empty string
-
-for i in range(4):
-    tmp+=a               # concatenating 'a' to 'tmp'
-    total+=int(tmp)      # converting string type to integer type
-
-print(total)
-
-"""
-OTHER VARIANT
+16th Task
 """
 
-a = input()
-total = int(a) + int(2*a) + int(3*a) + int(4*a)  # N*a=Na, for example  a="23", 2*a="2323",3*a="232323"
-print(total)
+numbers = input("Enter: ").split(',')
+odd_numbers = []
+for i in numbers:
+    if int(i) % 2 != 0:
+        odd_numbers.append(str(int(i) ** 2))
+print(','.join(odd_numbers))
+
+"""
+LIST COMPREHENSION
+"""
+odd_numbers = [str(int(i) ** 2) for i in numbers if int(i) % 2 != 0]
+print(','.join(odd_numbers))
+
+
