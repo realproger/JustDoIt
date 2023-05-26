8fd55a9fbd450d8f47c217cf7553eb59c3b04f8b
Tolbaev Muntasir <mtolbaev2000@gmail.com>
2023-05-26 12:58:53 +0500

    18th task

---
+++ Day5/questions.md
@@ -0,0 +1,38 @@
+# Question 18
+
+### **Question:**
+
+> **_A website requires the users to input username and password to register. Write a program to check the validity of password input by users._**
+
+> **_Following are the criteria for checking the password:_**
+
+- **_At least 1 letter between [a-z]_**
+- **_At least 1 number between [0-9]_**
+- **_At least 1 letter between [A-Z]_**
+- **_At least 1 character from [$#@]_**
+- **_Minimum length of transaction password: 6_**
+- **_Maximum length of transaction password: 12_**
+
+> **_Your program should accept a sequence of comma separated passwords and will check them according to the above criteria. Passwords that match the criteria are to be printed, each separated by a comma._**
+
+> **_Example_**
+
+> **_If the following passwords are given as input to the program:_**
+
+```
+ABd1234@1,a F1#,2w3E*,2We3345
+```
+
+> **_Then, the output of the program should be:_**
+
+```
+ABd1234@1
+```
+
+---
+
+### Hints:
+
+> **_In case of input data being supplied to the question, it should be assumed to be a console input._**
+
+---
---
+++ Day5/solutions.py
@@ -0,0 +1,56 @@
+"""
+18th Task
+"""
+def is_lower(x):
+    for i in x:
+        if 'a' <= i and i <= 'z':
+            return True
+    return False
+    
+def is_upper(x):
+    for i in x:
+        if 'A' <= i and i <= 'Z':
+            return True
+    return False
+
+def is_numeric(x):
+    for i in x:
+        if '0' <= i and i <= '9':
+            return True
+    return False
+
+def symbols(x):
+    for i in x:
+        if i == '$' or i == '@' or i == '#':
+            return True
+    return False
+
+def check_password():
+    password = input("Enter your password: ").split(',')
+    lst = []
+
+    for i in password:
+        length = len(i)
+        if 6 <= length and length <= 12 and is_lower(i) and is_upper(i) and is_numeric(i) and symbols(i):
+            lst.append(i)
+    return ','.join(lst)
+
+# print(check_password())
+
+""" WITH USING REGULAR EXPRESSIONS """
+import  re
+
+s = input().split(',')
+lst = []
+
+for i in s:
+    cnt = 0
+    cnt+=(6<=len(i) and len(i)<=12)
+    cnt+=bool(re.search("[a-z]",i))      # here re module includes a function re.search() which returns the object information
+    cnt+=bool(re.search("[A-Z]",i))      # of where the pattern string i is matched with any of the [a-z]/[A-z]/[0=9]/[@#$] characters
+    cnt+=bool(re.search("[0-9]",i))      # if not a single match found then returns NONE which converts to False in boolean
+    cnt+=bool(re.search("[@#$]",i))      # expression otherwise True if found any.
+    if cnt == 5:
+        lst.append(i)
+
+print(",".join(lst))
