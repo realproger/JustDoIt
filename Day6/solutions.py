4ee1c54f28f541bd13f0fe5ae02e9f8515486d55
Tolbaev Muntasir <mtolbaev2000@gmail.com>
2023-05-28 14:56:59 +0500

    22th task

--- Day6/solutions.py
+++ Day6/solutions.py
@@ -20,3 +20,34 @@ while True:
                             # N**P means N^P
 distance = round(math.sqrt(x**2 + y**2))       # euclidean distance = square root of (x^2+y^2) and rounding it to nearest integer
 print(distance)
+
+
+"""
+22th Task
+"""
+
+s_text = input("Enter: ").split()
+word = sorted(set(s_text))
+
+for i in word:
+    print(f"{i}:{s_text.count(i)}")
+
+""" ANOTHER VARIANT """
+ss = input().split()
+dict = {}
+for i in ss:
+    i = dict.setdefault(i,ss.count(i))    # setdefault() function takes key & value to set it as dictionary.
+
+dict = sorted(dict.items())               # items() function returns both key & value of dictionary as a list
+                                          # and then sorted. The sort by default occurs in order of 1st -> 2nd key
+for i in dict:
+    print("%s:%d"%(i[0],i[1]))
+
+""" ANOTHER VARIANT """
+
+text = input().split()
+dict_co = {i:text.count(i) for i in text}
+dict_co = sorted(dict_co.items())
+
+for i in dict_co:
+    print(f"{i[0]} : {i[1]}")
