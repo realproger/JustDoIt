de23adfb56163b70b984e2637107f9317cffad16
Tolbaev Muntasir <mtolbaev2000@gmail.com>
2023-06-05 12:34:41 +0500

    42nd task

--- Day10/questions.md
+++ Day10/questions.md
@@ -11,3 +11,17 @@
 > **_Use map() to generate a list.Use lambda to define anonymous functions._**
 
 ---
+
+# Question 42
+
+### **Question:**
+
+> **_Write a program which can map() and filter() to make a list whose elements are square of even number in [1,2,3,4,5,6,7,8,9,10]._**
+
+---
+
+### Hints:
+
+> **_Use map() to generate a list.Use filter() to filter elements of a list.Use lambda to define anonymous functions._**
+
+---
--- Day10/solutions.py
+++ Day10/solutions.py
@@ -3,3 +3,18 @@
 given_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 squared_nums = list(map(lambda x: x ** 2, given_list))
 print(squared_nums)
+
+""" 42nd Task """
+
+given_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+squared_even_nums = list(map(lambda x: x ** 2, filter(lambda x: x % 2 == 0, given_list)))
+print(squared_even_nums)
+
+def even(num):
+    return num % 2 == 0
+def squared(num):
+    return num ** 2
+
+li = [1,2,3,4,5,6,7,8,9,10]
+li = map(squared,filter(even,given_list))   # first filters number by even number and the apply map() on the resultant elements
+print(list(li))
